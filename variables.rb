0d88abf7f02e6b028bcd9c2a7a1a553fee2e4ac7
Jon Watkins <jkwatkins85@gmail.com>
2023-02-05 14:57:02 -0700

    Reassign Strings and Add Puts

--- variables.rb
+++ variables.rb
@@ -1,5 +1,5 @@
-first_name = "Jon"
-last_name = "Watkins"
+first_name = "First"
+last_name = "Last"
 email = "practiceemail@personal.com"
 age = 37
 pets = 1
@@ -8,3 +8,11 @@ deposit = 1200.00
 completion = 0.40
 deposit_paid = true
 started_mod1 = false
+
+first_name = "Jonathan"
+last_name = "Watkins"
+email = "jkwatkins85@gmail.com"
+
+puts age
+puts [first_name,last_name]
+puts "#{first_name}, you will be #{age + 3} years old by the end of 2025!"
